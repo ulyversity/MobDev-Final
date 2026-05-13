fac6ec81b666a7282f5df65ea6631b6229e3958d
AJGalario <24100050@usc.edu.ph>
2026-05-14 07:41:26 +0800

    AI hopefulyy works

--- app/src/main/java/ph/edu/usc24100050/PlannerActivity.java
+++ app/src/main/java/ph/edu/usc24100050/PlannerActivity.java
@@ -42,9 +42,7 @@ public class PlannerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_quick_planner);
 
         rvGetaways        = findViewById(R.id.rvGetaways);
-        rvUmikoItinerary  = findViewById(R.id.rvUmikoItinerary);
-        tvUmikoBanner     = findViewById(R.id.tvUmikoBanner);
-        tvUmikoEmpty      = findViewById(R.id.tvUmikoEmpty);
+       
 
         rvGetaways.setLayoutManager(new LinearLayoutManager(this));
         rvUmikoItinerary.setLayoutManager(new LinearLayoutManager(this));
